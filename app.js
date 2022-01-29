e537bd6d4afb860e68f58ccf0097e65b8e937999
VILLAVERDE <ovslash@hotmail.fr>
2022-01-29 10:33:52 +0100

    comit de depart - etape 3 semble ok

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+const userRoutes = require("./routes/user");
+
+
+mongoose
+  .connect(
+    "mongodb+srv://ovslash:10ZazattE51@cluster0.fplvd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
